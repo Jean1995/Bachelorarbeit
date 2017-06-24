b4c30022c6537fc7088879e442c2f426b101e936
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2017-06-25 01:07:40 +0200

    Die schlimmste Nacht meines Lebens. Vielleicht mit Happy End :)

--- imcrying/fit2.py
+++ imcrying/fit2.py
@@ -10,7 +10,7 @@ from numpy.linalg import inv
 import scipy.constants as const
 from numpy import random
 import uncertainties.unumpy as unp
-from uncertainties import correlated_values, covariance_matrix
+from uncertainties import correlated_values, covariance_matrix, correlation_matrix
 from table import (
     make_table,
     make_SI,
@@ -45,7 +45,8 @@ y = params.lattice_roh
 s_y = params.s_l
 V = params.V
 
-
+y_cor = np.array( correlated_values(y, V) ) # als np-array?
+print(y_cor)
 
 ### Funktionen
 
@@ -110,21 +111,23 @@ print("Designmatrix f+:")
 print(A)
 ### Gewichtsmatrix
 
-W = inv(V)
+W = inv(np.diag(np.diag(V))) # Nur die Gewichte! Kovarianz von uncertainties
 
 ### Berechnung der Parameter
 
 a_1 = inv(np.matmul(np.matmul(A.T, W), A))
-a_2 = np.matmul(np.matmul(A.T, W), y)
-a = np.matmul(a_1, a_2) # sollten 2*N-1 Werte sein.
+a_2 = np.dot(np.matmul(A.T, W), y_cor)
+a = np.dot(a_1, a_2) # sollten 2*N-1 Werte sein.
 a = np.insert(a, 0, a00(a)) # Berechne a_+0 aus den gegebenen Parametern
 
 ### Berechnung der Kovarianzmatrix der Parameter
 
-V = a_1
+V = covariance_matrix(a)
 
 
 ### Tada!
+
+a = unp.nominal_values(a)
 
 print("Parameter")
 print(a)
@@ -143,23 +146,24 @@ x_plot_p_down = np.zeros(len(x_plot))
 x_plot_n_up = np.zeros(len(x_plot))
 x_plot_n_down = np.zeros(len(x_plot))
 
-a_mc = random.multivariate_normal(np.delete(a,0), V, samples)
+
+a_mc = random.multivariate_normal(a, V, samples)
 
 #erstelle zufällige fehlerbehaftete Werte für Massen, Konstanten, etc.
 # Erstellung m_array und fehler siehe oben
 m_cov = np.diag(m_array_s)**2 # erstelle Kovarianzmatrix, jedoch ohne Kovarianzen (d.h. Varianzen auf Diagonalen)
 m_mc = random.multivariate_normal(m_array, m_cov, samples) # m_mc[a,b] mit b bestimmt ich, welche Variable ich erhalte (also b=0 m_p, usw.)
 
-a_00_vals = np.array([])
-for i in range(samples):
-    a_00_vals = np.insert(a_00_vals, 0, a00(a_mc[i,:]) ) # Ersetelle ein array mit a_+0 werten für alle samples
-
-a_mc = np.insert(a_mc, 0, a_00_vals, axis=1) # füge diese Spalte in die MonteCarlo-Daten ein
+#a_00_vals = np.array([])
+#for i in range(samples):
+#    a_00_vals = np.insert(a_00_vals, 0, a00(a_mc[i,:]) ) # Ersetelle ein array mit a_+0 werten für alle samples
+#
+#a_mc = np.insert(a_mc, 0, a_00_vals, axis=1) # füge diese Spalte in die MonteCarlo-Daten ein
 
 ### Werte ausgeben
 
 
-a_print = unp.uarray(a, np.insert(np.diag(V),1,np.std(a_00_vals))) # füge den Fehler von a00 aus Monte-Carlo ein. Ya
+a_print = unp.uarray(a, np.sqrt(np.diag(V))) # Wurzel der Varianzen, die auf der Diagonale stehen
 
 tab_label = []
 for i in range(N1):
@@ -205,17 +209,18 @@ for i, val in enumerate(x_plot):
 pred_lat_val_fp = np.zeros(len(x))
 for i in range(N1):
     pred_lat_val_fp = pred_lat_val_fp + a[i] * f(x, i, m_p)
-chi_squared_fp = np.sum((( pred_lat_val_fp - np.split(y,2)[0] ) / (np.split(s_y,2)[0]))**2)
+chi_squared_fp = np.sum((( pred_lat_val_fp - np.split(unp.nominal_values(y_cor),2)[0] ) / (np.split(unp.std_devs(y_cor),2)[0]))**2)
 
 #fn
 pred_lat_val_fn = np.zeros(len(x))
 for i in range(N2):
     pred_lat_val_fn = pred_lat_val_fn + a[i+N1] * f(x, i, m_0)
-chi_squared_fn = np.sum((( pred_lat_val_fn - np.split(y,2)[1] ) / (np.split(s_y,2)[1]))**2)
+chi_squared_fn = np.sum((( pred_lat_val_fn - np.split(unp.nominal_values(y_cor),2)[1] ) / (np.split(unp.std_devs(y_cor),2)[1]))**2)
 
 # Plot f+
 
-plt.errorbar( x, np.split(y,2)[0], yerr = np.split(s_y,2)[0], fmt=',', label=r'Lattice Calculations $f_+$', capsize=5,capthick=0.5, barsabove = True) # splitte in 2 Hälften und nehme die erste Hälfte
+
+plt.errorbar( x, np.split(unp.nominal_values(y_cor),2)[0], yerr = np.split(unp.std_devs(y_cor),2)[0], fmt=',', label=r'Lattice Calculations $f_+$', capsize=5,capthick=0.5, barsabove = True) # splitte in 2 Hälften und nehme die erste Hälfte
 y_plot_p = np.zeros(len(x_plot))
 for i in range(N1):
     y_plot_p = y_plot_p + a[i] * f(x_plot, i, m_p)
@@ -239,7 +244,7 @@ plt.clf()
 
 # Plot f0
 
-plt.errorbar( x, np.split(y,2)[1], yerr = np.split(s_y,2)[1], fmt=',', label=r'Lattice Calculations $f_0$', capsize=5,capthick=0.5, barsabove = True) # splitte in 2 Hälften und nehme die erste Hälfte
+plt.errorbar( x, np.split(unp.nominal_values(y_cor),2)[1], yerr = np.split(unp.std_devs(y_cor),2)[1], fmt=',', label=r'Lattice Calculations $f_0$', capsize=5,capthick=0.5, barsabove = True) # splitte in 2 Hälften und nehme die erste Hälfte
 y_plot_n = np.zeros(len(x_plot))
 for i in range(N2):
     y_plot_n = y_plot_n + a[i+N1] * f(x_plot, i, m_0)
